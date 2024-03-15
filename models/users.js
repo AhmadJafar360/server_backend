97b57489803f1f09ead1b256c8b366bebdfd1e08
Ahmad Jafar Ali <117719827+AhmadJafar360@users.noreply.github.com>
2024-03-16 04:36:26 +0700

    model_update

--- models/users.js
+++ models/users.js
@@ -1,23 +1,23 @@
 "use strict";
 const { Model } = require("sequelize");
-module.exports = {
-  async up(DataTypes, Sequelize) {
-    class Users extends Model {
-      /**
-       * Helper method for defining associations.
-       * This method is not a part of Sequelize lifecycle.
-       * The `models/index` file will call this method automatically.
-       */
-      static associate(models) {
-        // define association here
-      }
+module.exports = (sequelize, DataTypes) => {
+  class Users extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
     }
-    await DataTypes.init("users", {
+  }
+  Users.init(
+    {
       id: {
-        allowNull: false,
-        autoIncrement: true,
+        type: DataTypes.INTEGER,
         primaryKey: true,
-        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
       },
       firstname: DataTypes.STRING,
       lastname: DataTypes.STRING,
@@ -28,58 +28,11 @@ module.exports = {
       password: DataTypes.STRING,
       access_token: DataTypes.TEXT,
       refresh_token: DataTypes.TEXT,
-    });
-  },
-
-  async down(DataTypes, Sequelize) {
-    await DataTypes.dropTable("users");
-  },
+    },
+    {
+      sequelize,
+      modelName: "users",
+    }
+  );
+  return Users;
 };
-
-// module.exports = (sequelize, DataTypes) => {
-//   class Users extends Model {
-//     /**
-//      * Helper method for defining associations.
-//      * This method is not a part of Sequelize lifecycle.
-//      * The `models/index` file will call this method automatically.
-//      */
-//     static associate(models) {
-//       // define association here
-//     }
-//   }
-//   Users.init(
-//     id= {
-//       type: sequelize.INTEGER,
-//       primaryKey: true,
-//       autoIncrement: true,
-//       allowNull: false,
-//     },
-//     {
-
-//       firstname: DataTypes.STRING,
-//       lastname: DataTypes.STRING,
-//       gender: DataTypes.STRING,
-//       email: DataTypes.STRING,
-//       phone: DataTypes.STRING,
-//       birthdate: DataTypes.DATE,
-//       password: DataTypes.STRING,
-//       access_token: DataTypes.TEXT,
-//       refresh_token: DataTypes.TEXT,
-//     },
-//     {
-//       sequelize,
-//       modelName: "users",
-//     }
-//   );
-//   return Users;
-// };
-
-// firstname: DataTypes.STRING,
-// lastname: DataTypes.STRING,
-// gender: DataTypes.STRING,
-// email: DataTypes.STRING,
-// phone: DataTypes.STRING,
-// birthdate: DataTypes.DATE,
-// password: DataTypes.STRING,
-// access_token: DataTypes.TEXT,
-// refresh_token: DataTypes.TEXT,
