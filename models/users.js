825066ed163e755b763d97d6b83ed3969f46caa2
Ahmad Jafar Ali <117719827+AhmadJafar360@users.noreply.github.com>
2024-03-16 03:45:17 +0700

    Updated_model

--- models/users.js
+++ models/users.js
@@ -1,57 +1,43 @@
-import { Sequelize, DataTypes } from "sequelize";
+"use strict";
+const { Model } = require("sequelize");
+const db = require("../config/database");
+module.exports = (sequelize, DataTypes) => {
+  class Users extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  const Users = db.define("users", {
+    id: {
+      type: sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    firstname: DataTypes.STRING,
+    lastname: DataTypes.STRING,
+    gender: DataTypes.STRING,
+    email: DataTypes.STRING,
+    phone: DataTypes.STRING,
+    birthdate: DataTypes.DATE,
+    password: DataTypes.STRING,
+    access_token: DataTypes.TEXT,
+    refresh_token: DataTypes.TEXT,
+  });
+  return Users;
+};
 
-const sequelize = new Sequelize("mysql:root:VGNnltNafKcBycscnUiVElAFzoJjYbUq@monorail.proxy.rlwy.net:33259/railway");
-
-const User = sequelize.define("users", {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  firstname: DataTypes.STRING,
-  lastname: DataTypes.STRING,
-  gender: DataTypes.STRING,
-  email: DataTypes.STRING,
-  phone: DataTypes.STRING,
-  birthdate: DataTypes.DATE,
-  password: DataTypes.STRING,
-  access_token: DataTypes.TEXT,
-  refresh_token: DataTypes.TEXT,
-});
-
-export default User;
-
-// "use strict";
-// const {
-//   Model
-// } = require("sequelize");
-// module.exports = (sequelize, DataTypes) => {
-//   class Users extends Model {
-//     /**
-//      * Helper method for defining associations.
-//      * This method is not a part of Sequelize lifecycle.
-//      * The `models/index` file will call this method automatically.
-//      */
-//     static associate(models) {
-//       // define association here
-//     }
-//   }
-//   Users.init(
-//     {
-//       firstname: DataTypes.STRING,
-//       lastname: DataTypes.STRING,
-//       gender: DataTypes.STRING,
-//       email: DataTypes.STRING,
-//       phone: DataTypes.STRING,
-//       birthdate: DataTypes.DATE,
-//       password: DataTypes.STRING,
-//       access_token: DataTypes.TEXT,
-//       refresh_token: DataTypes.TEXT,
-//     },
-//     {
-//       sequelize,
-//       modelName: "users",
-//     }
-//   );
-//   return Users;
-// };
+// firstname: DataTypes.STRING,
+// lastname: DataTypes.STRING,
+// gender: DataTypes.STRING,
+// email: DataTypes.STRING,
+// phone: DataTypes.STRING,
+// birthdate: DataTypes.DATE,
+// password: DataTypes.STRING,
+// access_token: DataTypes.TEXT,
+// refresh_token: DataTypes.TEXT,
